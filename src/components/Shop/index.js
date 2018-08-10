06a184a56007f6b1c2ca2a4efbadd635be7d1e10
Duncan Leo <duncanleo97@gmail.com>
2018-08-10 21:12:07 +0800

    add basic geolocation

--- src/components/Shop/index.js
+++ src/components/Shop/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './styles.scss';
 
 import shopPropType from '../../prop-types/shop';
 
-const Shop = ({ shop }) => (
+const Shop = ({ shop, distance }) => (
   <div className="shop">
     <div className="chain">
       <span>
@@ -17,11 +18,20 @@ const Shop = ({ shop }) => (
     <span className="address">
       {shop.address}
     </span>
+    {
+      distance !== null ? (
+        <span className="distance">
+          {distance}
+          m
+        </span>
+      ) : null
+    }
   </div>
 );
 
 Shop.propTypes = {
   shop: shopPropType.isRequired,
+  distance: PropTypes.number.isRequired,
 };
 
 export default Shop;
--- src/components/ShopList/index.js
+++ src/components/ShopList/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { getDistance } from 'geolib';
 import Shop from '../Shop';
 
 import './styles.scss';
+
+const getShopLocation = (shop) => ({
+  latitude: shop.location.LATITUDE,
+  longitude: shop.location.LONGITUDE,
+});
 
 class ShopList extends Component {
   constructor(props) {
@@ -9,12 +15,16 @@ class ShopList extends Component {
 
     this.state = {
       shops: [],
+      location: null,
     };
 
     this.load = this.load.bind(this);
   }
 
   componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({ location: position.coords });
+    }, console.error);
     this.load();
   }
 
@@ -26,12 +36,20 @@ class ShopList extends Component {
   }
 
   render() {
-    const { shops } = this.state;
+    const { shops, location } = this.state;
+    const sortFunc = (prevShop, nextShop) => {
+      if (!location) {
+        return 0;
+      }
+      const prevShopDistance = getDistance(location, getShopLocation(prevShop));
+      const nextShopDistance = getDistance(location, getShopLocation(nextShop));
+      return prevShopDistance - nextShopDistance;
+    };
     return (
       <div className="shop-list">
         {
-          shops.map((shop) => (
-            <Shop shop={shop} key={shop.address} />
+          shops.sort(sortFunc).map((shop) => (
+            <Shop shop={shop} key={shop.address} distance={location ? getDistance(location, getShopLocation(shop)) : null} />
           ))
         }
       </div>
