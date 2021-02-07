997984d2035a14b5ed278402a03fae50c5f3c432
Duncan Leo <hello@duncanleo.me>
2021-02-07 14:39:54 +0800

    fix: public assets and HTML head elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 
 import Header from '../components/Header';
@@ -5,6 +6,21 @@ import ShopList from '../components/ShopList';
 
 const App = () => (
   <div>
+    <Head>
+      <title>Boba</title>
+      {/*Add favicon and webmanifest*/}
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="shortcut icon" href="/images/favicon.svg" type="image/svg" />
+      <link rel="shortcut icon" href="/images/favicon.png" type="image/png" />
+
+      {/*Support for iOS bookmarks and homescreen*/}
+      <link rel="apple-touch-icon" href="/images/boba-ios-192.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+      {/*Support for android OS*/}
+      <meta name="theme-color" content="#EAEDE6" />
+    </Head>
     <header>
       <Header />
     </header>
