fd477884f1fa2fcc9360d0e7cb44458e13f61252
Duncan Leo <hello@duncanleo.me>
2023-08-04 23:34:06 +0800

    chore: add Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'normalize.css';
 import '../stylesheets/styles.scss';
 
+import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
@@ -13,6 +14,7 @@ const App: React.FC<AppProps> = function (props) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 };
